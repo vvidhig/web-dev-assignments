b7ad289f8c4bf83fd03b84d668c1a9c9fa0c1d42
Vidhi <vidhidoesnotreply@gmail.com>
2024-12-19 09:50:26 +0530

    done

--- signin.js
+++ signin.js
@@ -3,3 +3,31 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
+
+const ALL_USERS = [
+    {
+      username: "harkirat@gmail.com",
+      password: "123",
+      name: "harkirat singh",
+    },
+    {
+      username: "raman@gmail.com",
+      password: "123321",
+      name: "Raman singh",
+    },
+    {
+      username: "priya@gmail.com",
+      password: "123321",
+      name: "Priya kumari",
+    },
+  ];
+
+  function userExist(username, password)
+  {
+    for(let i = 0; i < ALL_USERS.length; i++)
+    {
+        if(ALL_USERS[i].username === username && ALL_USERS[i].password === password)
+            return true;
+    }
+    return false;
+  }
